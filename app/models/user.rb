419bfa5ebb4e4faeb1bb9beb370d2a9c7c314400
vagrant <vagrant@rails-dev-box>
2013-11-25 04:48:44 +0000

    Interlude changes for registration, email permissions and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,10 @@ class User < ActiveRecord::Base
   def favorited(post)
     self.favorites.where(post_id: post.id).first
   end 
+
+  def voted(post)
+    self.votes.where(post_id: post.id).first
+  end
   
   # attr_accessible :title, :body
   private
